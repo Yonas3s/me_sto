3888611176f7947c7d5d26aff813de94e9fa7bb6
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-02-11 14:35:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let editbutton = document.querySelector('.editButton');
+let editbutton = document.querySelector('.profile__button_edit');
 let popup = document.querySelector(".popup");
-let closePopup = document.querySelector(".close_popup");
-let inputName = document.querySelector(".input-name");
-let inputJob = document.querySelector(".input-job");
+let closePopup = document.querySelector(".popup__button_close");
+let inputName = document.querySelector(".popup__input-name");
+let inputJob = document.querySelector(".popup__input-job");
 let placeName = document.querySelector(".profile__person_name");
 let placeJob = document.querySelector(".profile__person_job");
-let saveForm = document.querySelector(".save-form");
+let saveForm = document.querySelector(".popup__button_save");
 let profile = document.querySelector(".profile");
 let container = document.querySelector(".container");
-let likeButton = document.querySelector(".like_button");
+let likeButton = document.querySelector(".card__caption_like-button");
 
     function putAlike() {
         likeButton.style.setProperty('background-image', 'url("/images/Activelike.svg")')
